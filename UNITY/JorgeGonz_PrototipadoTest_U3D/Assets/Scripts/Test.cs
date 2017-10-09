69259a93f8c875aced67a4066fb2707df2d71b70
DARAKO-LAPTOP\Darako <darako.genesis@gmail.com>
2017-10-09 16:28:47 +0200

    Prototipado Test

--- UNITY/JorgeGonz_PrototipadoTest_U3D/Assets/Scripts/Test.cs
+++ UNITY/JorgeGonz_PrototipadoTest_U3D/Assets/Scripts/Test.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour {
+[Serializable]
+public struct TestWrapper {
+    [SerializeField]
+    public Test[] tests;
+}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[Serializable]
+public class Test {
+    [SerializeField]
+    public int idPregunta;
+    [SerializeField]
+    public string enunciado;
+    [SerializeField]
+    public string[] opciones;
+    [SerializeField]
+    public bool[] resultados;
+    [SerializeField]
+    public string explicacion;
+
+
+    public override string ToString() {
+        return "IdPregunta: " + this.idPregunta + "\nTitulo" + this.enunciado + "\nOpciones: " + this.opciones[0] + " - " + this.opciones[1] + "\nResultados: " + this.resultados[0] + " - " + this.resultados[1] + "\nExplicación: " + this.explicacion;
+    }
 }
---
+++ UNITY/JorgeGonz_PrototipadoTest_U3D/Assets/Scripts/TestManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestManager : MonoBehaviour {
+
+    public TestWrapper testWrapper;
+    private string pathResources = "Assets/Resources/";
+    private string nombreTest = "Test_prueba";
+    private string extension = ".json";
+
+    public Text textEnunciado;
+    public Button buttonOpcion0;
+    public Button buttonOpcion1;
+
+    private string textoAcierto = "¡HAS ACERTADO!";
+    private string textoError = "¡HAS FALLADO!";
+    public GameObject panelConfirmacion;
+    public Text textConfirmacion;
+    public Text textExplicacion;
+    public Button buttonConfirmacion;
+
+
+    void Start () {
+        //if (testWrapper.tests != null) {
+        //    SerializeMe(testWrapper, true);
+        //}
+        DeserializeMe();
+        InstanciarPregunta(SeleccionarPreguntaAleatoria());
+    }
+
+    void OnApplicationQuit() {
+        testWrapper.tests = null;    
+    }
+    public Test SeleccionarPreguntaAleatoria() {
+        int idPregunta = UnityEngine.Random.Range(0, testWrapper.tests.Length);
+        return testWrapper.tests[idPregunta];
+    }
+
+    public void InstanciarPregunta(Test test) {
+        textEnunciado.text = test.enunciado;
+        textExplicacion.text = test.explicacion;
+        buttonOpcion0.GetComponentInChildren<Text>().text = test.opciones[0];
+        buttonOpcion1.GetComponentInChildren<Text>().text = test.opciones[1];
+        buttonOpcion0.onClick.AddListener(() => ComprobarRespuesta(test.resultados[0]));
+        buttonOpcion1.onClick.AddListener(() => ComprobarRespuesta(test.resultados[1]));
+    }
+
+    public void ComprobarRespuesta(bool resultado) {
+        buttonOpcion0.interactable = false;
+        buttonOpcion1.interactable = false;
+        panelConfirmacion.SetActive(true);
+        if (resultado) {
+            textConfirmacion.text = textoAcierto;
+        } else {
+            textConfirmacion.text = textoError;
+        }
+        buttonConfirmacion.GetComponentInChildren<Text>().text = "OK";
+        buttonConfirmacion.onClick.AddListener(() => CerrarPanel());
+    }
+
+    public void CerrarPanel() {
+        buttonOpcion0.interactable = true;
+        buttonOpcion1.interactable = true;
+        panelConfirmacion.SetActive(false);
+    }
+
+    public void SerializeMe(TestWrapper testObject, bool saveToFile = false) {
+        string testJson = JsonUtility.ToJson(testObject);
+        Debug.Log(testJson);
+        if(saveToFile)
+            System.IO.File.WriteAllText(pathResources + nombreTest + extension, testJson);
+    }
+
+    public void DeserializeMe() {
+        string testJson = Resources.Load(nombreTest).ToString();
+        testWrapper = JsonUtility.FromJson<TestWrapper>(testJson);
+    }
+}
