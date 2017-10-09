4ec4ea0a0598aee6659487c365bb555abcf2d419
DARAKO-LAPTOP\Darako <darako.genesis@gmail.com>
2017-10-09 18:21:38 +0200

    putos git ignore

---
+++ UNITY/DontLoseTheNorth_U3D/Assets/PruebaClickable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaClickable : MonoBehaviour {
+
+   void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit) {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                if (hitInfo.transform.gameObject.tag == "Construction") {
+                    Debug.Log("It's working!");
+                } else {
+                    Debug.Log("nopz");
+                }
+            } else {
+                Debug.Log("No hit");
+            }
+            Debug.Log("Mouse is down");
+        }
+    }
+}
---
+++ UNITY/DontLoseTheNorth_U3D/Assets/Scripts/MovementManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour {
+
+    //0 -> ArribaDcha
+    //1 -> AbajoDcha
+    //2 -> AbajoIzq
+    //3 -> ArribaIzq
+    public void InstanceNextTile(GameObject nextTile, int posicion) {
+
+    }
+
+    public void TestMe() {
+        Debug.Log("test");
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+
+        }
+    }
+
+}
