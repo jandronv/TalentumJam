8ada030f057f85d6682b7233442dca34f08bef42
DARAKO-LAPTOP\Darako <darako.genesis@gmail.com>
2017-10-09 17:45:05 +0200

    PUTO COMMIT DE MIERDA

---
+++ UNITY/DontLoseTheNorth_U3D/Assets/Scripts/Quizz/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct TestWrapper {
+    [SerializeField]
+    public Test[] tests;
+}
+
+[Serializable]
+public class Test {
+    [SerializeField]
+    public int idPregunta;
+    [SerializeField]
+    public string enunciado;
+    [SerializeField]
+    public string[] opciones;
+    [SerializeField]
+    public bool[] resultados;
+    [SerializeField]
+    public string explicacion;
+
+
+    public override string ToString() {
+        return "IdPregunta: " + this.idPregunta + "\nTitulo" + this.enunciado + "\nOpciones: " + this.opciones[0] + " - " + this.opciones[1] + "\nResultados: " + this.resultados[0] + " - " + this.resultados[1] + "\nExplicación: " + this.explicacion;
+    }
+}
